5caa6fc5a238641cafb793483e8472067d19f755
Zhoukai <ZhouK9864@gmail.com>
2020-03-25 21:15:47 +0800

    Spring

--- src/test/java/com/Myclub/Test/Test.java
+++ src/test/java/com/Myclub/Test/Test.java
@@ -6,6 +6,7 @@ import com.Myclud.Domain.Account;
 import com.Myclud.Service.IAccountService;
 import config.SpringConfiguration;
 import org.junit.Before;
+import org.junit.runner.RunWith;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
@@ -18,24 +19,30 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.ResolvableType;
 import org.springframework.core.env.Environment;
-import org.springframework.core.io.Resource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
+@RunWith(SpringJUnit4ClassRunner.class)  //把原有的junit提供的main方法换成spring提供的
+@ContextConfiguration(classes = SpringConfiguration.class) //告知spring的运行器，ioc和spring的创建是基于xml的还是注解的 xml用locations
 public class Test {
     Account account = new Account();
+
+    @Resource
     IAccountService as;
 
-    @Before
-    public void init(){
-        ApplicationContext ac = new ClassPathXmlApplicationContext("Bean.xml");  //xml配置
-//        ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);   //注解配置
-        as = ac.getBean("accountService",IAccountService.class);
-    }
+//    @Before
+//    public void init(){
+//        ApplicationContext ac = new ClassPathXmlApplicationContext("Bean.xml");  //xml配置
+//       ApplicationContext ac = new AnnotationConfigApplicationContext(SpringConfiguration.class);   //注解配置
+//        as = ac.getBean("accountService",IAccountService.class);
+//    }
 
     @org.junit.Test
     public void testFiandAll() {
